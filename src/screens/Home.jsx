815bde04829b20eb981eeebf14fafe2831986d18
Nallis02 <75026477+Nallis02@users.noreply.github.com>
2022-10-07 20:01:51 -0500

    Adelantando funciones de crud firestore

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,33 @@
 import { AuthContext } from "../context/AuthContext";
-import { setData, usersCol } from "../utils/database";
+import {  productsCol, setData,  deleteDocument, inventarioCol, docSnap } from "../utils/database";
 
 export default function Home() {
   const { saludo } = AuthContext();
-  const data = { id: "Camila3", data: "Camila5" };
-  const onClick = () => setData(usersCol, data);
+  const data = { id: "2", data: "PC" };
+  const onClick = () => setData(productsCol, data);
+  
+  const data1 = {  id: "knlLnnn2GUB8rme6lC1h"};
+  const remove = () => deleteDocument(inventarioCol, data1);
+  // const buscarId = {  id: "xyNPIxSlAVUxPCogFKW2"};
+  // const buscarPorId = () => buscaerXId(inventarioCol, buscarId)
+  const buscarPorId = ()=> {
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+    } else {
+      console.log("no existe");
+    }
+  }
+
+  // const buscarSnap = () => buscarSnapshot(usersCol)
+
   return (
     <section>
       <div>{saludo.nombre}</div>
       <button onClick={onClick}>Añadir Usuario</button>
+      <button onClick={remove}>Eliminar</button>
+      <button onClick={buscarPorId}>Buscar Por Id</button>
+      {/* <button onClick={buscarSnap}>Buscar coleccion Snapshot</button> */}
+      
     </section>
   );
 }
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,10 +1,40 @@
-import { collection, doc, setDoc } from "firebase/firestore";
+// import { query } from "express";
+import { addDoc, collection, deleteDoc, doc, getDoc, onSnapshot, setDoc, query, where,orderBy, limit } from "firebase/firestore";
 import { db } from "../services/firebase";
 
 // objects Collection
 export const usersCol = collection(db, "users");
 export const productsCol = collection(db, "productos");
+export const inventarioCol = collection(db, "inventario");
 
 // setter function
 export const setData = (col, data) =>
   setDoc(doc(col, data.id), data, { merge: true });
+//funcion para agregar coleccion sin id especifico
+export const setDocument = (col, data) => addDoc(col, { data });
+
+export const deleteDocument = (col, data) => deleteDoc(doc(col, data.id),data);
+
+// export const buscaerXId = (col, data)=> getDoc(doc(col, data.id),data);
+const docRef = doc(inventarioCol, "xyNPIxSlAVUxPCogFKW2");
+export const docSnap = await getDoc(docRef);
+
+// - Leer (todos) documentos por colección con get
+
+//Peticion asincrona axios
+// - Leer (todos) documentos por colección con snapshot
+// export const buscarSnapshot = (col) => {
+//   onSnapshot(doc(col))
+// };
+
+export const buscarSnapshot = onSnapshot(doc(usersCol, '2'), (doc) => {
+  console.log("Current data: ", doc.data());
+});
+// - Leer (filtrar x campo) documentos por colección con query y get
+// const q = query(usersCol, where("name", "==", 'camila'), orderBy("name"));
+
+// - Leer (filtrar x campo) documentos por colección con query y snapshot
+
+// - Investigar datos que se pueden recolectar al manejar bases de datos
+
+
